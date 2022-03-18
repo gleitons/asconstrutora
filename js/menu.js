58057b0bc659df49642ff63e0dd3c8425b845cba
gleitons <31367377+gleitons@users.noreply.github.com>
2022-03-18 15:32:25 -0300

    em construcao

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+document.getElementById('menuP').innerHTML = `<li><a href="/">Inicio</a></li>
+<li><a href="sobre.html">Sobre</a></li>
+<li class="has-sub">
+    <div class="drop-down-menu">
+        <a href="servicos.html">Serviços</a>
+        <div class="dropdown-menu-wrap">
+            <ul>
+                <li><a href="#">Preparação do Projeto</a></li>
+                <li><a href="#">Levanta</a></li>
+                <li><a href="#">Graphic Design</a></li>
+                <li><a href="#">Branding</a></li>
+            </ul>
+        </div>
+    </div>
+</li>
+<li><a href="portfolio.html">Portfolio</a></li>
+<li class="has-sub">
+    <div class="drop-down-menu">
+        <a href="#">Sidebars</a>
+        <div class="dropdown-menu-wrap">
+            <ul>
+                <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                
+            </ul>
+        </div>
+    </div>
+</li>
+<li class="cta"><a href="contact.html">Contato</a></li>`
